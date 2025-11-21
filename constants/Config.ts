b4de55f41931abe521afa9967b7d837f711e6758
John Steven Libradilla <125227974+StevenLibradilla1728@users.noreply.github.com>
2025-11-21 01:24:40 -0800

    Update Config.ts

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,9 +1,9 @@
 // constants/Config.ts
-//Mobilephone IP address - 10.20.180.147
+
 
 // --- CHANGE YOUR IP ADDRESS HERE ONLY ---
 // Make sure to keep the ':8080' if your XAMPP is running on that port.
 // If XAMPP is on default port 80, remove ':8080'.
-// export const API_BASE_URL = 'http://192.168.1.199:8080/caps/TuklasLunasAPI/api';
+// export const API_BASE_URL = 'http://YourIP/caps/TuklasLunasAPI/api';
 
-export const API_BASE_URL = 'http://10.221.126.147:8080/caps/TuklasLunasAPI/api';
+export const API_BASE_URL = 'http://YourIP/caps/TuklasLunasAPI/api';
